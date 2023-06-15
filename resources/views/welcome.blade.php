5fecb2f3232d9944494b24558e3dc385bfee5964
Maicon Ferreira <maiconrcf@gmail.com>
2023-06-15 09:45:55 -0300

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -59,7 +59,7 @@
 
                 <div class="mt-16 text-center">                    
                     <div class="grid grid-cols-1 md:grid-cols-1 gap-6 lg:gap-8">
-                        <div class="scale-100 p-6 bg-white from-gray-700/50 via-transparent rounded-lg shadow-2xl shadow-gray-500/20 flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500 text-center" style="max-width: 700px;">
+                        <div class="" style="max-width: 550px;">
                             <div>
                                 <img src="{{ asset('/img/whatshook-banner.svg') }}" width="100%" class="">
 
